6e741034822fa5448285d024284ef59893d1aadc
Juliano da Silva Simas <juliano.developer01@gmail.com>
2020-03-29 20:56:04 -0300

    back engenharia e licenciamento

--- src/main/java/com/prolagos/sispcbackend/repositories/ProjLicenciamentosRepository.java
+++ src/main/java/com/prolagos/sispcbackend/repositories/ProjLicenciamentosRepository.java
@@ -1,10 +1,21 @@
 package com.prolagos.sispcbackend.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.prolagos.sispcbackend.domain.Cad_Projetos_Licenciamentos;
 
 @Repository
 public interface ProjLicenciamentosRepository extends JpaRepository<Cad_Projetos_Licenciamentos, Integer> {
+	
+	@Transactional(readOnly=true)
+	@Query("SELECT obj FROM Cad_Projetos_Licenciamentos obj WHERE obj.projetoId.projetoId = :id ORDER BY obj.licencaId")
+	public List<Cad_Projetos_Licenciamentos> licenPorProjeto(@Param("id") Integer id);
+
+	
 }

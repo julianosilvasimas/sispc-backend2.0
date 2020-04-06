4ffb54c6e946525223ea5ae05f1a5b43bbbec58b
Juliano da Silva Simas <juliano.developer01@gmail.com>
2020-04-06 17:38:03 -0300

    Relações comprovações

--- src/main/java/com/prolagos/sispcbackend/domain/Cad_Projetos_ComprovacaoArquivos.java
+++ src/main/java/com/prolagos/sispcbackend/domain/Cad_Projetos_ComprovacaoArquivos.java
@@ -3,6 +3,7 @@ package com.prolagos.sispcbackend.domain;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
@@ -11,6 +12,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -25,10 +29,13 @@ public class Cad_Projetos_ComprovacaoArquivos implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Getter @Setter private Integer comprovacaoarquivoId;
 	@Getter @Setter private String nomearquivo;
 	@Getter @Setter private String caminho;
+	@Getter @Setter private String tipoarquivo;
+	@Column(nullable=true)
+	@JsonDeserialize(using = LocalDateDeserializer.class) 
 	@Getter @Setter private LocalDate envio;
 	
 	@ManyToOne
